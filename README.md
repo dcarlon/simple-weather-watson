a8f98a753fe15ff9a022a4ace3b4511e56ed6e49
Armando Sanchez <armando.sanchez.franco@ibm.com>
2018-04-05 16:07:15 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# simple-nodejs-weather-app
+Weather Application with Watson for Bluemix 
+
+* Check out the **[Live Demo]()**
+
+* Run the web app locally:
+```
+node server.js
+// Now open your browser and visit: localhost:3000
+```
---
+++ settings.js
@@ -0,0 +1,5 @@
+exports.watson_text_speach_username = "3bbaa794-430f-452a-9662-4328326cb2c1";
+exports.watson_text_speach_password = "BHb7S6kXLviU";
+exports.watson_text_speach_url = "https://stream.watsonplatform.net/text-to-speech/api/";
+
+exports.weather_key = "199c0a6f1b66f772acf7ed686b8c602a";
